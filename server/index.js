59b92e9084591da0ef8430bac41340437a611c5b
Askat B <Askat@MacBook-Pro-2.home>
2020-02-28 23:42:51 -0500

    server production deploy config

--- server/index.js
+++ server/index.js
@@ -36,4 +36,14 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 app.use("/api/posts", posts);
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
