58bf343e25cd146b21c2eab1d936bf6c2c21ef20
Askat B <Askat@MacBook-Pro-2.home>
2020-02-29 00:43:36 -0500

    server.js deploy config

--- server/index.js
+++ server/index.js
@@ -39,7 +39,7 @@ app.use("/api/posts", posts);
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("../client/build"));
+  app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
