41063fd7fea5d6de4d850b57a6e8569b1be53b76
Askat B <Askat@MacBook-Pro-2.home>
2020-03-01 11:13:51 -0500

    heroku daployment

--- server/index.js
+++ server/index.js
@@ -39,12 +39,11 @@ app.use("/api/posts", posts);
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static(path.resolve(__dirname, "../", "client", "build")));
+  app.use(express.static(("client/build"));
 
+  // html file for all routes
   app.get("*", (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, "../", "client", "build", "index.html")
-    );
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
 }
 
