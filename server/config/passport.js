17d589d5fc7a3ec7aa8f31904d13438ff110a4a5
Askat B <Askat@MacBook-Pro-2.home>
2020-03-01 13:25:44 -0500

    heroku daployment

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,12 +1,13 @@
 const jwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 const User = require("../models/user");
+const keys = require("./keys");
 
 require("dotenv").config();
 
 let opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = process.env.SECRET;
+opts.secretOrKey = keys.secretOrKey;
 
 module.exports = passport => {
   passport.use(
