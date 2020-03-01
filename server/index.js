5e4a7f6459fe26798f2ff74a6c5fcdd53defe91f
Askat B <Askat@MacBook-Pro-2.home>
2020-03-01 02:20:12 -0500

    package.json added

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
+const path = require("path");
 const express = require("express");
-const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const passport = require("passport");
@@ -8,7 +8,7 @@ const users = require("./routes/users");
 const posts = require("./routes/posts");
 
 // Setup enviroment
-dotenv.config();
+require("dotenv").config();
 
 // Server PORT
 const PORT = process.env.PORT || 1234;
@@ -39,10 +39,12 @@ app.use("/api/posts", posts);
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("client/build"));
+  app.use(express.static(path.resolve(__dirname, "../", "client", "build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(
+      path.resolve(__dirname, "../", "client", "build", "index.html")
+    );
   });
 }
 
