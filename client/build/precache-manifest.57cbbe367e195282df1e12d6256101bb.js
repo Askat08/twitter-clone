957431a4613453be7336bb174f814388707c79e9
Askat B <Askat@MacBook-Pro-2.home>
2020-03-03 22:32:28 -0500

    deploy issue

---
+++ client/build/precache-manifest.57cbbe367e195282df1e12d6256101bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff5f0062e54a4cab8e645aef1bc2ffb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e92f51feabb1a277a56",
+    "url": "/static/css/main.37ba812b.chunk.css"
+  },
+  {
+    "revision": "5d0f04f2f0f38aae987b",
+    "url": "/static/js/2.157516d7.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.157516d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e92f51feabb1a277a56",
+    "url": "/static/js/main.dab96f56.chunk.js"
+  },
+  {
+    "revision": "cda345fb50b7e11c0025",
+    "url": "/static/js/runtime-main.844668cf.js"
+  }
+]);
